bb7f6031d92962c96a0281537535f184b013c907
NishantShah <nishant@genesesolution.com>
2020-11-26 14:03:02 +0545

    add user routes and schema and connected to cloud.mongodb.com and used express-async-handler package and function to the route

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 import express from "express";
 import { data } from "./data.js";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
+
+const pwd = "g1dPr5MkQp7nVjzx";
 
 const app = express();
 const port = process.env.PORT || 5001;
+mongoose.connect(
+  "mongodb+srv://admin:g1dPr5MkQp7nVjzx@cluster0.zxbdw.mongodb.net/amazona?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  () => console.log("Connected to DB")
+);
+
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send("Server is Ready now");
@@ -20,6 +35,10 @@ app.get("/api/products/:id", (req, res) => {
 
 app.get("/api/products", (req, res) => {
   res.status(200).send(data.products);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 app.listen(port, () => console.log("Server running at port " + port));
